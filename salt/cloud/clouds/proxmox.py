d1a31a8aef20fc48e975c0fa7ffb88d91a86c5c3
Denys Duchier <denys.duchier@univ-orleans.fr>
2014-11-24 20:59:35 +0100

    don't try to grab a vm's disk detail that's missing (kvm)

--- salt/cloud/clouds/proxmox.py
+++ salt/cloud/clouds/proxmox.py
@@ -412,7 +412,8 @@ def list_nodes(call=None):
         ret[vm_name] = {}
         ret[vm_name]['id'] = str(vm_details['vmid'])
         ret[vm_name]['image'] = str(vm_details['vmid'])
-        ret[vm_name]['size'] = str(vm_details['config']['disk'])
+        if 'disk' in vm_details['config']:
+            ret[vm_name]['size'] = str(vm_details['config']['disk'])
         ret[vm_name]['state'] = str(vm_details['status'])
 
         # Figure out which is which to put it in the right column
